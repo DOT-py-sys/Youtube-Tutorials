3c5d8d9b74b8604e17cffb281bc73e8d9ffb4902
Koushik Dutta <74253021+DOT-py-sys@users.noreply.github.com>
2023-01-04 01:56:25 +0530

    Create class_one.py

---
+++ python modules/Modules/class_one.py
@@ -0,0 +1,22 @@
+
+# Public
+variable_public = 10
+
+# Protected Variable
+_variable_protected = 11
+
+# Private Variable
+__variable_private = 12
+
+class sample:
+    @staticmethod
+    def test():
+        print("Hey There")
+
+class sample_two:
+    @staticmethod
+    def __test():
+        print("Hey There")
+
+def _mthod():
+    print('hey i m using method')
